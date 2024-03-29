aaba490efcd414cb859ca8cb28466ecb63752ed6
seojw4973 <sgw0816@naver.com>
2024-03-29 20:13:36 +0900

    _27_MemberListServlet_Including MemberList.jsp에 Header.jsp, Tail.jsp를 인클루딩 방식으로 포함 forward 방식으로 할 경우 포워딩 받은 서블릿이 응답의 주체가되어, 다른 서블릿은 화면에 표현되지 않음

---
+++ _27_MemberListServlet_Including/src/main/java/spms/servlets/MemberListServlet.java
@@ -0,0 +1,120 @@
+package spms.servlets;
+
+import java.io.IOException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import spms.vo.Member;
+
+@WebServlet("/member/list")
+@SuppressWarnings("serial")
+public class MemberListServlet extends HttpServlet{
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("MemberListServlet::doGet() 호출");
+
+		Connection conn = null;		// DB 서버와의 연결 객체
+		Statement stmt = null;		// sql문
+		ResultSet rs = null;		// Select문의 결과
+		
+		try {
+			ServletContext sc = this.getServletContext();
+			Class.forName(sc.getInitParameter("driver"));
+			conn = DriverManager.getConnection(
+					sc.getInitParameter("url"),
+					sc.getInitParameter("username"),
+					sc.getInitParameter("password")
+					);
+			stmt = conn.createStatement();
+			// DB 테이블 전체 리스트를 가져와서 rs에 저장한 상태
+			rs = stmt.executeQuery("SELECT mno, mname, email, cre_date" +
+								" FROM members" + 
+								" ORDER BY mno ASC");
+			/* 회원 목록을 list 객체로 생성
+			 * Memberlist.jsp를 호출하면서 list 객체를 전달 
+			 * */
+			List<Member> members = new ArrayList<>();
+			// rs = null;	// 예외를 발생시키기 위해서 고의로 null값 부여
+			while(rs.next()) {
+				members.add(new Member()
+								.setNo(rs.getInt("mno"))
+								.setName(rs.getString("mname"))
+								.setEmail(rs.getString("email"))
+								.setCreatedDate(rs.getDate("cre_date"))
+						);
+			}
+			// request 공간에 members라는 key 값으로 list를 저장한다.
+			// 그러면 Memberlist.jsp에서는 members라는 key값으로 꺼낼 수 있다.
+			req.setAttribute("members", members);
+			
+			// MemberListServlet객체에서 MemberList.jsp로
+			// request객체와 response객체를 전달한다.
+			
+			RequestDispatcher rd = req.getRequestDispatcher(
+							"/member/MemberList.jsp");
+			res.setContentType("text/html;charset=UTF-8");
+			
+			/* 
+			 * 서블릿이나 jsp에서 다른 서블릿/jsp로 이동하는 방법
+			 * 1) redirect : 브라우저한테 다른 주소로 새로 접속해, request를 공유할 수 없다.(기존 request를 가지고 요청하는 것이 아닌 새로운 요청이므로)
+			 * 				브라우저의 url창은 기존 주소로 접속했다가, 새로운 주소로 표시된다.
+			 * 2) 기존 request를 전달하는 요청
+			 * 	: 기존 request가 아직 브라우저에 응답으로 가지않고,
+			 * 	  서버 내부에서 페이지 경로 이동만 하는 방식
+			 * 	  * 브라우저가 처음 접속한 주소가 브라우저에 그대로 표현된다.
+			 * 		(다만 서버 내부에서는 경로를 이동)
+			 * 	  a) include : 일시 호출하고 다시 원래 호출한 곳으로 제어권이 넘어온다.
+			 * 					MemberListServlet -> MemberList.jsp -> MemberListServlet 응답 -> Browser
+			 * 	  b) forward : 호출과 동시에 브라우저에 응답 제어권을 넘긴다.
+			 * 					MemberListServlet -> MemberList.jsp 응답 -> Browser
+			 * */
+			// include방식으로 전달한다.
+			rd.include(req, res);
+//			rd.forward(req, res);
+			
+			/*
+			 * html 코드와 html 코드에 java값을 전달하는 부분
+			res.setContentType("text/html;charset=UTF-8");
+			PrintWriter out = res.getWriter();
+			out.println("<html><head><title>회원 목록</title></head>");
+			out.println("<body><h1>회원 목록</h1>");
+			out.println("<p><a href='add'>신규 회원</a></p>");
+			while(rs.next()) {
+				out.println(
+						rs.getInt("mno") + ", " +
+						"<a href='update?no=" + rs.getInt("mno") + "'>" +
+						rs.getString("mname") + "</a>, " +
+						rs.getString("email") + ", " +
+						rs.getDate("cre_date") +
+						"<a href='delete?no=" + rs.getInt("mno") + "'>" +"[삭제]</a>" + "<br>"
+						);
+			}
+			out.println("</body></html>");
+			*/
+		}catch(Exception e) {
+//			throw new ServletException(e);
+			req.setAttribute("error", e.getMessage());
+			RequestDispatcher rd = req.getRequestDispatcher("/Error.jsp");
+			rd.forward(req, res);
+//			req.setAttribute("errorEx", e);
+		}finally {
+			try {if(rs!=null) rs.close();} catch(Exception e) {}
+			try {if(stmt!=null) rs.close();} catch(Exception e) {}
+			try {if(conn!=null) rs.close();} catch(Exception e) {}
+		}
+	}
+
+}
