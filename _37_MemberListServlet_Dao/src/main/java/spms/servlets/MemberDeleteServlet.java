a5a51d5d509fde57bf31be87be76b278aca90416
seojw4973 <sgw0816@naver.com>
2024-03-29 21:29:19 +0900

    _37_MemberListServlet_Dao sql문 전부 작성 후 MemberListServlet에서 MemberDao객체 생성해서 기능 구현

---
+++ _37_MemberListServlet_Dao/src/main/java/spms/servlets/MemberDeleteServlet.java
@@ -0,0 +1,62 @@
+package spms.servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member/delete")
+@SuppressWarnings("serial")
+public class MemberDeleteServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("MemberDeleteServlet::doGet() 호출");
+		req.setCharacterEncoding("UTF-8");
+		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+		try {
+			ServletContext sc = this.getServletContext();
+			Class.forName(sc.getInitParameter("driver"));
+			conn = DriverManager.getConnection(
+//					"jdbc:mysql://localhost:4306/studydb",	// JDBC url
+//					"study",								// id
+//					"study");								// password
+					sc.getInitParameter("url"),
+					sc.getInitParameter("username"),
+					sc.getInitParameter("password")
+					);
+			
+			stmt = conn.prepareStatement(
+					"DELETE FROM members WHERE mno=?");
+			stmt.setInt(1, Integer.parseInt(req.getParameter("no")));
+			stmt.executeUpdate();
+			
+			resp.sendRedirect("list");
+			
+			
+			
+		}catch (Exception e){
+			e.printStackTrace();
+			req.setAttribute("error", e);
+			RequestDispatcher rd = req.getRequestDispatcher("/Error.jsp");
+			rd.forward(req, resp);
+//			throw new ServletException(e);
+		}finally {
+			try { if(stmt!=null) stmt.close();} catch(Exception e) {}
+			try { if(conn!=null) conn.close();} catch(Exception e) {}
+		}
+	
+	}
+	
+}
---
+++ _37_MemberListServlet_Dao/src/main/java/spms/servlets/MemberListServlet.java
@@ -0,0 +1,76 @@
+package spms.servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import spms.dao.MemberDao;
+import spms.vo.Member;
+
+
+
+@WebServlet("/member/list")
+@SuppressWarnings("serial")
+public class MemberListServlet extends HttpServlet{
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("MemberListServlet::doGet() 호출");
+
+		Connection conn = null;		
+		Statement stmt = null;		
+		ResultSet rs = null;		
+		
+		try {
+			ServletContext sc = this.getServletContext();
+			conn = (Connection)sc.getAttribute("conn");
+			
+			MemberDao memberDao = new MemberDao();
+			memberDao.setConnection(conn);
+			List<Member> members = memberDao.selectList();
+			
+			/*
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("SELECT mno, mname, email, cre_date" +
+								" FROM members" + 
+								" ORDER BY mno ASC");
+			
+			List<Member> members = new ArrayList<>();
+			while(rs.next()) {
+				members.add(new Member()
+								.setNo(rs.getInt("mno"))
+								.setName(rs.getString("mname"))
+								.setEmail(rs.getString("email"))
+								.setCreatedDate(rs.getDate("cre_date"))
+						);
+			}
+			*/
+						
+			req.setAttribute("members", members);
+			
+			
+			RequestDispatcher rd = req.getRequestDispatcher(
+							"/member/MemberList.jsp");
+			res.setContentType("text/html;charset=UTF-8");
+			
+			rd.include(req, res);
+		}catch(Exception e) {
+			req.setAttribute("error", e);
+			RequestDispatcher rd = req.getRequestDispatcher("/Error.jsp");
+			rd.forward(req, res);
+		}finally {
+			try {if(rs!=null) rs.close();} catch(Exception e) {}
+			try {if(stmt!=null) rs.close();} catch(Exception e) {}
+		}
+	}
+
+}
