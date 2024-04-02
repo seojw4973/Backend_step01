d933797dffc8172cb08a0df9a7a042dd034602bf
seojw4973 <sgw0816@naver.com>
2024-04-02 21:03:31 +0900

    _45_DispatcherServlet 프론트 컨트롤러 패턴을 사용하여, 공통 로직은 프론트 컨트롤러에서 실행하고, 프론트 컨트롤러는 각 요청에 맞는 컨트롤러를 찾아서 호출시킴

---
+++ _45_DispatchServlet/src/main/java/spms/servlets/MemberAddServlet.java
@@ -0,0 +1,78 @@
+package spms.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import spms.dao.MemberDao;
+import spms.vo.Member;
+
+@WebServlet("/member/add")
+@SuppressWarnings("serial")
+public class MemberAddServlet extends HttpServlet{
+	
+	// get : 화면에 정보나 페이지를 보여주는 역할
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("MemberAddServlet::doGet() 호출");
+		
+		RequestDispatcher rd = req.getRequestDispatcher("/member/MemberForm.jsp");
+		rd.forward(req, resp);
+	}
+	
+	// post : 주로 정보를 전달받아서 db에 저장하는 역할
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("MemberAddServlet::doPost() 호출");
+
+//		Connection conn = null;
+//
+//		PreparedStatement stmt = null;
+		
+		try {
+			ServletContext sc = this.getServletContext();
+			
+			MemberDao memberDao = (MemberDao)sc.getAttribute("memberDao");
+			
+			memberDao.insert(new Member()
+					.setEmail(req.getParameter("email"))
+					.setName(req.getParameter("name"))
+					.setPassword(req.getParameter("password")));
+			
+			resp.sendRedirect("list");
+			
+
+		}catch(Exception e) {
+			e.printStackTrace();
+			req.setAttribute("error", e);
+			RequestDispatcher rd = req.getRequestDispatcher("/Error.jsp");
+			rd.forward(req, resp);
+		}
+		/*
+		finally {
+			// Connection 객체를 1개 생성해서 ServletContext 영역에 보관하고
+			// 모든 Servlet이 공유해서 사용하므로 이곳에서 닫으면 안된다.
+			try {if(stmt!=null) stmt.close();} catch(Exception e){}
+		}
+		*/
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ _45_DispatchServlet/src/main/webapp/el/el01.jsp
@@ -0,0 +1,51 @@
+<%@page import="spms.vo.Member"%>
+<%@page import="java.util.HashMap"%>
+<%@page import="java.util.Map"%>
+<%@page import="java.util.List"%>
+<%@page import="java.util.LinkedList"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>EL(Expression Language)</title>
+<style type="text/css">
+body {
+  font-size: small;
+}
+table {
+  border: 			thin solid gray;
+  border-collapse: 	collapse;
+}
+
+td, th {
+  border:           thin dotted gray;
+  padding:          2px;
+}
+
+th {
+  background-color: lightgray;
+}
+
+pre {
+  font-size: 90%;
+}
+</style>
+</head>
+<body>
+<p><a href="el02.jsp">[다음]</a></p>
+<h2>EL - 리터럴 표현식</h2>
+<table>
+<tr><th>데이터 형</th><th>EL 코드</th><th>실행 결과</th></tr>
+<tr><td>문자열</td><td>\${"test"}</td><td>${"test"}</td></tr>
+<tr><td>문자열</td><td>\${'test'}</td><td>${'test'}</td></tr>
+<tr><td>정수</td><td>\${20}</td><td>${20}</td></tr>
+<tr><td>부동소수점</td><td>\${3.14}</td><td>${3.14}</td></tr>
+<tr><td>불린</td><td>\${true}</td><td>${true}</td></tr>
+<tr><td>널 값</td><td>\${null}</td><td>${null}</td></tr>
+</table>
+
+<p><a href="el02.jsp">[다음]</a></p>
+</body>
+</html>
